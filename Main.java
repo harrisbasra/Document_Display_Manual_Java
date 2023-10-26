805b8c8d1a49fc14d87da0fff585790ee3d48645
Harris Basra <harrisbasra@gmail.com>
2023-10-26 13:08:55 +0500

    Section Class Working

--- Main.java
+++ Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Document{
+    private String title;
+    List<Section> sections;
+    Document(String t){
+        title = t;
+        sections = new ArrayList<>();
+    }
+    public void display(){
+        for(int i=0;i<sections.size();i++){
+            sections.get(i).display();
+            System.out.println("");
+        }
+    }
+    public void add(Section s){
+        sections.add(s);
+    }
 }
+
+
+abstract class Section{
+    protected String content;
+    Section(String c){
+        content = c;
+    }
+    public void display(){}
+}
+
